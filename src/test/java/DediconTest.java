73b7a5079fc0667e34ddb26e17570483d86a9179
Bert Frees <bertfrees@gmail.com>
2015-09-29 14:22:22 +0200

    Add an example formatter test

--- src/test/java/DediconTest.java
+++ src/test/java/DediconTest.java
@@ -11,6 +11,7 @@ import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
 import static org.daisy.pipeline.pax.exam.Options.forThisPlatform;
 import static org.daisy.pipeline.pax.exam.Options.logbackBundles;
 import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
+import static org.daisy.pipeline.pax.exam.Options.pipelineModule;
 import static org.daisy.pipeline.pax.exam.Options.thisBundle;
 import static org.daisy.pipeline.pax.exam.Options.xprocspecBundles;
 
@@ -32,7 +33,7 @@ import static org.ops4j.pax.exam.CoreOptions.options;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-public class TranslationTest {
+public class DediconTest {
 	
 	@Inject
 	private XProcSpecRunner xprocspecRunner;
@@ -60,15 +61,26 @@ public class TranslationTest {
 			mavenBundle().groupId("org.daisy.bindings").artifactId("jhyphen").versionAsInProject(),
 			mavenBundle().groupId("org.liblouis").artifactId("liblouis-java").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.braille").artifactId("braille-utils.api").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.braille").artifactId("braille-utils.pef-tools").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.braille").artifactId("braille-utils.impl").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.libs").artifactId("jing").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.libs").artifactId("jstyleparser").versionAsInProject(),
 			mavenBundle().groupId("org.unbescape").artifactId("unbescape").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.braille").artifactId("braille-css").versionAsInProject(),
 			mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.antlr-runtime").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.api").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.common").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.formatter.impl").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.task-api").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.task.impl").versionAsInProject(),
 			brailleModule("common-utils"),
 			brailleModule("css-core"),
 			brailleModule("css-calabash"),
 			brailleModule("css-utils"),
 			brailleModule("pef-core"),
+			brailleModule("pef-calabash"),
+			brailleModule("pef-saxon"),
+			brailleModule("pef-utils"),
 			brailleModule("liblouis-core"),
 			brailleModule("liblouis-saxon"),
 			brailleModule("liblouis-calabash"),
@@ -78,6 +90,13 @@ public class TranslationTest {
 			brailleModule("libhyphen-core"),
 			brailleModule("libhyphen-libreoffice-tables"),
 			onWindows ? null : forThisPlatform(brailleModule("libhyphen-native")),
+			brailleModule("dotify-core"),
+			brailleModule("dotify-saxon"),
+			brailleModule("dotify-calabash"),
+			brailleModule("dotify-utils"),
+			brailleModule("dotify-formatter"),
+			pipelineModule("file-utils"),
+			pipelineModule("common-utils"),
 			thisBundle(),
 			xprocspecBundles(),
 			junitBundles()
