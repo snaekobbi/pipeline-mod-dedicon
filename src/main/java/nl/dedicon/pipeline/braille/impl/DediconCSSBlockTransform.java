851a3d7d122ee360403e7f84cc7e414f411a0453
Bert Frees <bertfrees@gmail.com>
2015-10-16 10:05:55 +0200

    Base on mod-braille 1.9.6

--- src/main/java/nl/dedicon/pipeline/braille/impl/DediconCSSBlockTransform.java
+++ src/main/java/nl/dedicon/pipeline/braille/impl/DediconCSSBlockTransform.java
@@ -4,30 +4,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.net.URI;
 import javax.xml.namespace.QName;
 
-import static com.google.common.base.Objects.toStringHelper;
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
-import static com.google.common.collect.Iterables.concat;
-import static com.google.common.collect.Iterables.transform;
-import static com.google.common.collect.Lists.newArrayList;
 
 import static org.daisy.pipeline.braille.css.Query.parseQuery;
 import static org.daisy.pipeline.braille.common.util.Tuple3;
 import static org.daisy.pipeline.braille.common.util.URIs.asURI;
+import org.daisy.pipeline.braille.common.AbstractTransform;
+import org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Function;
+import org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables.concat;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables.transform;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.logCreate;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.logSelect;
 import org.daisy.pipeline.braille.common.CSSBlockTransform;
 import org.daisy.pipeline.braille.common.Hyphenator;
+import org.daisy.pipeline.braille.common.TextTransform;
 import org.daisy.pipeline.braille.common.Transform;
-import org.daisy.pipeline.braille.common.Transform.AbstractTransform;
 import static org.daisy.pipeline.braille.common.Transform.Provider.util.dispatch;
-import static org.daisy.pipeline.braille.common.Transform.Provider.util.logCreate;
-import static org.daisy.pipeline.braille.common.Transform.Provider.util.logSelect;
 import static org.daisy.pipeline.braille.common.Transform.Provider.util.memoize;
 import static org.daisy.pipeline.braille.common.util.Locales.parseLocale;
-import org.daisy.pipeline.braille.common.WithSideEffect;
 import org.daisy.pipeline.braille.common.XProcTransform;
 import org.daisy.pipeline.braille.liblouis.LiblouisTranslator;
 
@@ -38,8 +38,6 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.ComponentContext;
 
-import org.slf4j.Logger;
-
 public interface DediconCSSBlockTransform extends CSSBlockTransform, XProcTransform {
 	
 	@Component(
@@ -49,7 +47,8 @@ public interface DediconCSSBlockTransform extends CSSBlockTransform, XProcTransf
 			CSSBlockTransform.Provider.class
 		}
 	)
-	public class Provider implements XProcTransform.Provider<DediconCSSBlockTransform>, CSSBlockTransform.Provider<DediconCSSBlockTransform> {
+	public class Provider extends AbstractTransform.Provider<DediconCSSBlockTransform>
+		                  implements XProcTransform.Provider<DediconCSSBlockTransform>, CSSBlockTransform.Provider<DediconCSSBlockTransform> {
 		
 		private URI href;
 		
@@ -58,6 +57,14 @@ public interface DediconCSSBlockTransform extends CSSBlockTransform, XProcTransf
 			href = asURI(context.getBundleContext().getBundle().getEntry("xml/block-translate.xpl"));
 		}
 		
+		private final static String liblouisTable = "(liblouis-table:'" +
+			"http://www.dedicon.nl/liblouis/nl-NL-g0.utb,http://www.dedicon.nl/liblouis/undefined.utb')";
+		private final static String hyphenationTable = "(libhyphen-table:'http://www.libreoffice.org/dictionaries/hyphen/hyph_nl_NL.dic')";
+		private final static String fallbackHyphenationTable = "(hyphenator:tex)(locale:nl-NL)";
+		
+		private final static Iterable<DediconCSSBlockTransform> empty
+		= Iterables.<DediconCSSBlockTransform>empty();
+		
 		/**
 		 * Recognized features:
 		 *
@@ -65,89 +72,62 @@ public interface DediconCSSBlockTransform extends CSSBlockTransform, XProcTransf
 		 * - locale: Will only match if the language subtag is 'nl'.
 		 *
 		 */
-		public Iterable<DediconCSSBlockTransform> get(String query) {
-			 return impl.get(query);
-		 }
-	
-		public Transform.Provider<DediconCSSBlockTransform> withContext(Logger context) {
-			return impl.withContext(context);
-		}
-	
-		private Transform.Provider.MemoizingProvider<DediconCSSBlockTransform> impl = new ProviderImpl(null);
-	
-		private class ProviderImpl extends AbstractProvider<DediconCSSBlockTransform> {
-			
-			private final static String liblouisTable = "(liblouis-table:'" +
-				"http://www.dedicon.nl/liblouis/nl-NL-g0.utb,http://www.dedicon.nl/liblouis/undefined.utb')";
-			private final static String hyphenationTable = "(libhyphen-table:'http://www.libreoffice.org/dictionaries/hyphen/hyph_nl_NL.dic')";
-			private final static String fallbackHyphenationTable = "(hyphenator:tex)(locale:nl-NL)";
-		
-			private ProviderImpl(Logger context) {
-				super(context);
-			}
-		
-			protected Transform.Provider.MemoizingProvider<DediconCSSBlockTransform> _withContext(Logger context) {
-				return new ProviderImpl(context);
-			}
-		
-			protected final Iterable<WithSideEffect<DediconCSSBlockTransform,Logger>> __get(String query) {
-				Map<String,Optional<String>> q = new HashMap<String,Optional<String>>(parseQuery(query));
-				Optional<String> o;
-				if ((o = q.remove("locale")) != null)
-					if (!"nl".equals(parseLocale(o.get()).getLanguage()))
-						return empty;
-				if ((o = q.remove("translator")) != null)
-					if (o.get().equals("dedicon"))
-						if (q.size() == 0) {
-							Iterable<WithSideEffect<Hyphenator,Logger>> hyphenators = concat(
-								logSelect(hyphenationTable, hyphenatorProvider.get(hyphenationTable)),
-								logSelect(fallbackHyphenationTable, hyphenatorProvider.get(fallbackHyphenationTable)));
-							return transform(
+		protected final Iterable<DediconCSSBlockTransform> _get(String query) {
+			Map<String,Optional<String>> q = new HashMap<String,Optional<String>>(parseQuery(query));
+			Optional<String> o;
+			if ((o = q.remove("locale")) != null)
+				if (!"nl".equals(parseLocale(o.get()).getLanguage()))
+					return empty;
+			if ((o = q.remove("translator")) != null)
+				if (o.get().equals("dedicon"))
+					if (q.size() == 0) {
+						Iterable<Hyphenator> hyphenators = concat(
+							logSelect(hyphenationTable, hyphenatorProvider),
+							logSelect(fallbackHyphenationTable, hyphenatorProvider));
+						return concat(
+							transform(
 								concat(
-									transform(
-										hyphenators,
-										new WithSideEffect.Function<Hyphenator,String,Logger>() {
-											public String _apply(Hyphenator h) {
-												return h.getIdentifier(); }}),
-									newArrayList(
-										WithSideEffect.<String,Logger>of("liblouis"),
-										WithSideEffect.<String,Logger>of("none"))),
-								new WithSideEffect.Function<String,DediconCSSBlockTransform,Logger>() {
-									public DediconCSSBlockTransform _apply(String hyphenator) {
-										String translatorQuery = liblouisTable + "(hyphenator:" + hyphenator + ")";
-										try {
-											applyWithSideEffect(
-												logSelect(
-													translatorQuery,
-													liblouisTranslatorProvider.get(translatorQuery)).iterator().next()); }
-										catch (NoSuchElementException e) {
-											throw new NoSuchElementException(); }
-										return applyWithSideEffect(
-											logCreate(
-												(DediconCSSBlockTransform)new TransformImpl(translatorQuery))); }}); }
+									concat(
+										transform(
+											hyphenators,
+											new Function<Hyphenator,String>() {
+												public String _apply(Hyphenator h) {
+													return h.getIdentifier(); }}),
+										"liblouis"),
+									"none"),
+								new Function<String,Iterable<DediconCSSBlockTransform>>() {
+									public Iterable<DediconCSSBlockTransform> _apply(String hyphenator) {
+										final String translatorQuery = liblouisTable + "(hyphenator:" + hyphenator + ")";
+										return transform(
+											logSelect(translatorQuery, liblouisTranslatorProvider),
+											new Function<LiblouisTranslator,DediconCSSBlockTransform>() {
+												public DediconCSSBlockTransform _apply(LiblouisTranslator translator) {
+													return __apply(logCreate(new TransformImpl(translatorQuery, translator))); }}); }})); }
 				return empty;
-			}
 		}
-	
-		private final static Iterable<WithSideEffect<DediconCSSBlockTransform,Logger>> empty
-		= Optional.<WithSideEffect<DediconCSSBlockTransform,Logger>>absent().asSet();
 		
 		private class TransformImpl extends AbstractTransform implements DediconCSSBlockTransform {
 			
+			private final LiblouisTranslator translator;
 			private final Tuple3<URI,QName,Map<String,String>> xproc;
 			
-			private TransformImpl(String translatorQuery) {
+			private TransformImpl(String translatorQuery, LiblouisTranslator translator) {
 				Map<String,String> options = ImmutableMap.of("query", translatorQuery);
 				xproc = new Tuple3<URI,QName,Map<String,String>>(href, null, options);
+				this.translator = translator;
 			}
-	
+			
+			public TextTransform asTextTransform() {
+				return translator;
+			}
+			
 			public Tuple3<URI,QName,Map<String,String>> asXProc() {
 				return xproc;
 			}
-	
+			
 			@Override
 			public String toString() {
-				return toStringHelper(DediconCSSBlockTransform.class.getSimpleName())
+				return Objects.toStringHelper(DediconCSSBlockTransform.class.getSimpleName())
 					.toString();
 			}
 		}
@@ -180,11 +160,14 @@ public interface DediconCSSBlockTransform extends CSSBlockTransform, XProcTransf
 			cardinality = ReferenceCardinality.MULTIPLE,
 			policy = ReferencePolicy.DYNAMIC
 		)
-		protected void bindHyphenatorProvider(Hyphenator.Provider provider) {
-			hyphenatorProviders.add(provider);
+		@SuppressWarnings(
+			"unchecked" // safe cast to Transform.Provider<Hyphenator>
+		)
+		protected void bindHyphenatorProvider(Hyphenator.Provider<?> provider) {
+			hyphenatorProviders.add((Transform.Provider<Hyphenator>)provider);
 		}
 	
-		protected void unbindHyphenatorProvider(Hyphenator.Provider provider) {
+		protected void unbindHyphenatorProvider(Hyphenator.Provider<?> provider) {
 			hyphenatorProviders.remove(provider);
 			hyphenatorProvider.invalidateCache();
 		}
