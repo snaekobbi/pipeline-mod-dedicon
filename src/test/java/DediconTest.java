23055f0fc9f8f9b3f59b95d23ff379fc7b65032c
Paul Rambags <paulrambags@dedicon.nl>
2017-08-25 14:23:16 +0200

    Simplified DediconTest (in the same way as pipeline-braille-scripts/dtbook-to-pef/src/test/java/XProcSpecTest.java) Parent pipeline-mod-braille 1.10.1

--- src/test/java/DediconTest.java
+++ src/test/java/DediconTest.java
@@ -1,94 +1,23 @@
-import javax.inject.Inject;
+import org.daisy.pipeline.junit.AbstractXSpecAndXProcSpecTest;
 
-import java.io.File;
+import static org.daisy.pipeline.pax.exam.Options.thisPlatform;
 
-import com.google.common.collect.ImmutableMap;
-
-import org.daisy.maven.xproc.xprocspec.XProcSpecRunner;
-
-import static org.daisy.pipeline.pax.exam.Options.brailleModule;
-import static org.daisy.pipeline.pax.exam.Options.calabashConfigFile;
-import static org.daisy.pipeline.pax.exam.Options.domTraversalPackage;
-import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
-import static org.daisy.pipeline.pax.exam.Options.logbackClassic;
-import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
-import static org.daisy.pipeline.pax.exam.Options.mavenBundle;
-import static org.daisy.pipeline.pax.exam.Options.mavenBundlesWithDependencies;
-import static org.daisy.pipeline.pax.exam.Options.thisBundle;
-import static org.daisy.pipeline.pax.exam.Options.xprocspec;
-import static org.ops4j.pax.exam.CoreOptions.systemPackage;
-
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.ops4j.pax.exam.Configuration;
-import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
-import org.ops4j.pax.exam.spi.reactors.PerClass;
-import org.ops4j.pax.exam.util.PathUtils;
-
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
-
-@RunWith(PaxExam.class)
-@ExamReactorStrategy(PerClass.class)
-public class DediconTest {
+public class DediconTest extends AbstractXSpecAndXProcSpecTest {
 	
-	@Inject
-	private XProcSpecRunner xprocspecRunner;
-	
-	@Test
-	public void runXProcSpec() throws Exception {
-		File baseDir = new File(PathUtils.getBaseDir());
-		boolean success = xprocspecRunner.run(ImmutableMap.of(
-			                                      "test_css_formatter.xprocspec",
-			                                      new File(baseDir, "src/test/xprocspec/test_css_formatter.xprocspec"),
-			                                      "test_css_translator.xprocspec",
-			                                      new File(baseDir, "src/test/xprocspec/test_css_translator.xprocspec"),
-			                                      "test_script.xprocspec",
-			                                      new File(baseDir, "src/test/xprocspec/test_script.xprocspec"),
-			                                      "test_translator.xprocspec",
-			                                      new File(baseDir, "src/test/xprocspec/test_translator.xprocspec")
-			                                      ),
-		                                      new File(baseDir, "target/xprocspec-reports"),
-		                                      new File(baseDir, "target/surefire-reports"),
-		                                      new File(baseDir, "target/xprocspec"),
-		                                      null,
-		                                      new XProcSpecRunner.Reporter.DefaultReporter());
-		assertTrue("XProcSpec tests should run with success", success);
-	}
-	
-	@Configuration
-	public Option[] config() {
-		return options(
-			systemProperty("file.encoding").value("UTF8"),
-			systemPackage("javax.xml.stream;version=\"1.0.1\""),
-			logbackConfigFile(),
-			domTraversalPackage(),
-			calabashConfigFile(),
-			felixDeclarativeServices(),
-			thisBundle(),
-			junitBundles(),
-			mavenBundlesWithDependencies(
-				brailleModule("common-utils"),
-				brailleModule("css-utils"),
-				brailleModule("liblouis-utils"),
-				brailleModule("liblouis-tables"),
-				brailleModule("liblouis-native").forThisPlatform(),
-				brailleModule("libhyphen-core"),
-				brailleModule("libhyphen-libreoffice-tables"),
-				brailleModule("libhyphen-native").forThisPlatform(),
-				brailleModule("dotify-formatter"),
-				brailleModule("pef-utils"),
-				brailleModule("dtbook-to-pef"),
-				// logging
-				logbackClassic(),
-				// xprocspec
-				xprocspec(),
-				mavenBundle("org.daisy.maven:xproc-engine-daisy-pipeline:?"))
-		);
+	@Override
+	protected String[] testDependencies() {
+		return new String[] {
+                    brailleModule("common-utils"),
+                    brailleModule("css-utils"),
+                    brailleModule("liblouis-utils"),
+                    brailleModule("liblouis-tables"),
+                    "org.daisy.pipeline.modules.braille:liblouis-native:jar:" + thisPlatform() + ":?",
+                    brailleModule("libhyphen-core"),
+                    brailleModule("libhyphen-libreoffice-tables"),
+                    "org.daisy.pipeline.modules.braille:libhyphen-native:jar:" + thisPlatform() + ":?",
+                    brailleModule("dotify-formatter"),
+                    brailleModule("pef-utils"),
+                    brailleModule("dtbook-to-pef")
+                };
 	}
 }
